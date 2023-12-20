4b8b984db56794c063d38b15d95c1b05a1e69334
Ngov-Julie-RESTART <julie.ngov@edu.devinci.fr>
2023-12-20 10:18:23 +0100

    fix center mascotte galery and change title names

--- mascotte.php
+++ mascotte.php
@@ -28,7 +28,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Mascotte</title>
     <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
