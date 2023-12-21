195a7a1997e3f96e706dff07d098266c8cc4b944
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2023-12-21 13:44:11 +0100

    :sparkles: edit button close QR code

--- connec.php
+++ connec.php
@@ -1,8 +1,7 @@
 <?php
     try{
-        $database = new PDO("mysql:host=localhost;dbname=mascotte", "root","");
+        $database = new PDO("mysql:host=localhost;dbname=mascotte", "root","root");
     } catch(Exception $e){
         die("Erreur :" . $e->getMessage());
     }
-    
 ?>
